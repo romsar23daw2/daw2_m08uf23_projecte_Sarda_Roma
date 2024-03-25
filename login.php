7c150fd9aa703956015a801562f26ce37c49b39f
romasarca <w9tdango98osaw@proton.me>
2024-03-25 17:49:58 +0000

    v.2 - Added menu.

--- login.php
+++ login.php
@@ -1,15 +1,18 @@
 <html>
-	<head>
-		<title>
-			AUTENTICANT AMB LDAP DE L'USUARI admin
-		</title>
-	</head>
-	<body>
-		<form action="http://zend-rosaca.fjeclot.net/autent/auth.php" method="POST">
-			Usuari amb permisos d'administració LDAP: <input type="text" name="adm"><br>
-			Contrasenya de l'usuari: <input type="password" name="cts"><br>
-			<input type="submit" value="Envia" />
-			<input type="reset" value="Neteja" />
-		</form>
-	</body>
+
+<head>
+	<title>
+		Login
+	</title>
+</head>
+
+<body>
+	<form action="http://zend-rosaca.fjeclot.net/autent/auth.php" method="POST">
+		Usuari amb permisos d'administració LDAP: <input type="text" name="adm"><br>
+		Contrasenya de l'usuari: <input type="password" name="cts"><br>
+		<input type="submit" value="Envia" />
+		<input type="reset" value="Neteja" />
+	</form>
+</body>
+
 </html>
--- menu.php
+++ menu.php
@@ -1,12 +1,18 @@
 <html>
-	<head>
-		<title>
-			PÀGINA WEB DEL MENÚ PRINCIPAL DE L'APLICACIÓ D'ACCÉS A BASES DE DADES LDAP
-		</title>
-	</head>
-	<body>
-		<h2> MENÚ PRINCIPAL DE L'APLICACIÓ D'ACCÉS A BASES DE DADES LDAP</h2>
-		<h3> <b>En construcció!!!!!!!!!!!</b> </h3>
-		<a href="http://zend-rosaca.fjeclot.net/autent/index.php">Torna a la pàgina inicial</a>
-	</body>
+
+<head>
+	<title>
+		PÀGINA WEB DEL MENÚ PRINCIPAL DE L'APLICACIÓ D'ACCÉS A BASES DE DADES LDAP
+	</title>
+</head>
+
+<body>
+	<h2> MENÚ PRINCIPAL DE L'APLICACIÓ D'ACCÉS A BASES DE DADES LDAP</h2>
+	<a href="http://zend-rosaca.fjeclot.net/zendldap/consulta.php">Consultar Usuari</a> <br>
+	<a href="http://zend-rosaca.fjeclot.net/zendldap/crea.html">Crear Usuari</a> <br>
+	<a href="http://zend-rosaca.fjeclot.net/zendldap/edita.html">Editar Usuari</a> <br>
+	<a href="http://zend-rosaca.fjeclot.net/zendldap/elimina.html">Eliminar Usuari</a> <br>
+	<a href="http://zend-rosaca.fjeclot.net/autent/index.php">Torna a la pàgina inicial</a>
+</body>
+
 </html>
